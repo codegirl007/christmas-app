e02c6abc1c9d6015da0e4b310403f66c5040e563
Marketa Krucka <marketakrucka@gmail.com>
2021-11-27 16:52:18 +0100

    added more christmas carols

--- src/components/Audio/index.jsx
+++ src/components/Audio/index.jsx
@@ -10,6 +10,12 @@ import rocarol from "../../assets/music/ro-carol.mp3"
 import rucarol from "../../assets/music/ru-carol.mp3"
 import uscarol from "../../assets/music/us-carol.mp3"
 import zacarol from "../../assets/music/za-carol.mp3"
+import brcarol from "../../assets/music/br-carol.mp3"
+import cncarol from "../../assets/music/cn-carol.mp3"
+import czcarol from "../../assets/music/cz-carol.mp3"
+import frcarol from "../../assets/music/fr-carol.mp3"
+import incarol from "../../assets/music/in-carol.mp3"
+import uacarol from "../../assets/music/ua-carol.mp3"
 
 const carols = {
   AU: aucarol,
@@ -22,6 +28,12 @@ const carols = {
   RU: rucarol,
   US: uscarol,
   ZA: zacarol,
+  BR: brcarol,
+  CN: cncarol,
+  CZ: czcarol,
+  FR: frcarol,
+  IN: incarol,
+  UA: uacarol,
 }
 
 
