98d09a9e0a56ca55cf18eaa5e0b4aa3fcbca2716
Jana Koutníková <janca.koutnikova@gmail.com>
2021-11-27 12:29:56 +0100

    Dialogs Styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
-import Layout from './components/Layout/Layout';
-import MainPage from './components/MainPage'
-import countries from './assets/data/data.json';
+import Layout from "./components/Layout/Layout";
+import MainPage from "./components/MainPage";
+import countries from "./assets/data/data.json";
 import "./App.css";
-
+import { ChristmasDialog } from "./components/Dialog/Dialog";
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <MainPage/>
+        <MainPage />
       </Layout>
+      <ChristmasDialog />
     </div>
   );
 }
--- src/components/Dialog/Dialog.jsx
+++ src/components/Dialog/Dialog.jsx
@@ -6,42 +6,62 @@ import choir from "../../assets/icons/choir.png";
 import jesus from "../../assets/icons/jesus.png";
 import language from "../../assets/icons/language.png";
 import "./Dialog.css";
+import AudioPlayer from "material-ui-audio-player";
 
 export const ChristmasDialog = () => {
   return (
-    <>
-      <Dialog open={true} className="dialog-body">
+    <Dialog open={true} sx={{ borderRadius: 2 }}>
+      <div className="dialog-container">
         <DialogTitle className="dialog-header">Czech Republic, CZ</DialogTitle>
         <DialogContent className="dialog-content">
           <div className="fact">
-            <img src={gift} alt="giver" />
-            <p>Jezisek (Baby Jesus) is a Christmas gift-giving figure</p>
+            <div className="img-container">
+              <img src={gift} alt="giver" />
+            </div>
+            <p className="fact-text">
+              Jezisek (Baby Jesus) is a Christmas gift-giving figure
+            </p>
           </div>
           <div className="fact">
-            <img src={dinner} alt="dish" />
-            <p>Carp and Potato Salad</p>
+            <div className="img-container">
+              <img src={dinner} alt="dish" />
+            </div>
+            <p className="fact-text">Carp and Potato Salad</p>
           </div>
           <div className="fact">
-            <img src={calendar} alt="calendar" />
-            <p>December 24</p>
+            <div className="img-container">
+              <img src={calendar} alt="calendar" />
+            </div>
+            <p className="fact-text">December 24</p>
           </div>
           <div className="fact">
-            <img src={choir} alt="carol" />
-            <p>Veselé Vánoce!</p>
+            <div className="img-container">
+              <img src={language} alt="language" />
+            </div>
+            <p className="fact-text">Veselé Vánoce!</p>
           </div>
-          <div className="fact">
-            <img src={jesus} alt="tradition" />
-            <p>
+          <div className="fact-row">
+            <div className="img-container">
+              <img src={jesus} alt="tradition" />
+            </div>
+            <p className="fact-text">
               The Floating of Walnut Shells, The Cutting of the Apple, The
               Pouring of Lead, Fish Scales under the Plate
             </p>
           </div>
-          <div className="fact">
-            <img src={language} alt="language" />
+          <div className="fact-row">
+            <div className="img-container">
+              <img src={choir} alt="carol" />
+            </div>
+            <AudioPlayer
+              src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+              width="60%"
+              height="0%"
+            />
           </div>
           <div className="social-media"></div>
         </DialogContent>
-      </Dialog>
-    </>
+      </div>
+    </Dialog>
   );
 };
